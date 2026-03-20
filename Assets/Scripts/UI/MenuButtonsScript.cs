76409871df7b6e56206885b626ea8043e9aa56f5
Павел Жилин <lzhilinchik@gmail.com>
2026-03-20 20:24:03 +0300

    UI added/updated
    -донастроил главное меню -донастроил паузу в игре -забиндил кнопки со скриптами

---
+++ Assets/Scripts/UI/MenuButtonsScript.cs
@@ -0,0 +1,12 @@
+using UnityEngine;
+
+namespace Jam.UI
+{
+    public class MenuButtonsScript : MonoBehaviour
+    {
+        public void QuitGame()
+        {
+            Application.Quit();
+        }
+    }
+}
